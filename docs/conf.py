bbe038df1e5f39cda802a1cf85fbe98e9012e3f0
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-06-28 09:18:00 +0200

    Run test with py35

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+# otherwise, readthedocs.io uses their theme by default, so no need to specify it
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
